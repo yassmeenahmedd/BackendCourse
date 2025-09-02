f5085359f9b1af633908662d42a649f8efde92bf
yasmen fahmy <129455756+yassmeenahmedd@users.noreply.github.com>
2025-09-02 22:26:19 +0300

    Add files via upload

---
+++ MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using mango.Services.CouponAPI.Models;
+using mango.Services.CouponAPI.Models.DTO;
+
+namespace mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+
+                config.CreateMap<Coupon, CouponDTO>();
+                config.CreateMap<CouponDTO, Coupon>();
+            });
+           
+            return mappingConfig;
+        }
+    }
+}
